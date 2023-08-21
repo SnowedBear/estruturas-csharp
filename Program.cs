be5e4fc7d70aad80ee3f8ab630e7756d66b695cc
Snowed Bear <brunocanesso31@gmail.com>
2023-08-21 14:46:29 +0000

    Estrutura Numero Aleatorio

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EstruturasCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            
+            Console.WriteLine("Estou pensando em um numero de 1 a 10, qual numero é esse?");
+             int numeroAleatorio = rnd.Next(1, 11);
+            
+            while (true)
+            {
+               int resposta;
+                resposta = int.Parse(Console.ReadLine());
+
+                if(resposta == numeroAleatorio)
+                {
+                    Console.WriteLine("Parabens! era o numero " + numeroAleatorio);
+                    break;
+                }
+                else if (resposta != numeroAleatorio)
+                {
+                    Console.WriteLine("Errou! Tente denovo!");
+                }
+            }
+        }
+    }
+}
